432c67d1f70f9bfda204c865def617efd9acfb16
newt <axelskam.123@outlook.es>
2023-06-09 23:59:20 -0500

    chore(Root): test sui mono

--- apps/server/src/modules/authentication/authentication.service.ts
+++ apps/server/src/modules/authentication/authentication.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthenticationService {
   getUser(): string {
-    return 'found user daaa';
+    return 'found user';
   }
 }
